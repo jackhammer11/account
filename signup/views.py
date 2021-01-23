0c03783a78563e94db62542d7cf43fc9590d314c
Ranjeet Khartode <ranjeet@leometric.com>
2021-01-23 18:37:42 +0530

    changed view

--- signup/views.py
+++ signup/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-
+from .form import SignUpForm
 
 
 
@@ -12,14 +12,24 @@ def home_view(request):
 
 
 def signup_view(request):
-	form = UserCreationForm(request.POST)
+	form = SignUpForm(request.POST)
 	if form.is_valid():
-		form.save()
+		user = form.save()
+		user.refresh_from_db()
+		user.profile.first_name = form.cleaned_data.get('first_name')
+		user.profile.last_name= form.cleaned_data.get('last_name')
+		user.profile.email= form.cleaned_data.get('email')
+
+		user.save()
+
 		username = form.cleaned_data.get('username')
-		password  = form.cleaned_data.get('password1')
-		user = authenticate(user=username, password=password)
+		password = form.cleaned_data.get('password1')
+		user = authenticate(username=username, password=password)
 		login(request,user)
 
 		return redirect('home')
+
+	else:
+		form = SignUpForm()
 
 	return render(request,'signup/signup.html',{'form':form})
